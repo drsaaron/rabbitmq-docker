4fd167f68f195b8ee5f7812a97c0ab7620e9c531
Scott Aaron <dr_saaron@yahoo.com>
2023-12-14 16:03:03 -0600

    add container to network

--- createContainer.sh
+++ createContainer.sh
@@ -6,14 +6,22 @@ containerName=rabbit
 vhostName=my-rabbit
 DATA_DIR=data
 
+case $(uname) in
+    Linux)
+	network=qotd
+	;;
+    *)
+	network=blazarnetwork
+esac
+
 [ -d $DATA_DIR ] || mkdir $DATA_DIR
 
-if pullLatestDocker.sh -i $imageName
+if true #pullLatestDocker.sh -i $imageName
 then
     docker stop $containerName
     docker rm $containerName
     
-    docker run -d --hostname $vhostName --name $containerName -p 8080:15672 -p 5672:5672 -v `pwd`/$DATA_DIR:/var/lib/rabbitmq/mnesia/rabbit@$vhostName $imageName
+    docker run -d --hostname $vhostName --name $containerName -p 8080:15672 -p 5672:5672 -v `pwd`/$DATA_DIR:/var/lib/rabbitmq/mnesia/rabbit@$vhostName --network $network $imageName
 else
     echo "image already latest, so no update"
 fi
